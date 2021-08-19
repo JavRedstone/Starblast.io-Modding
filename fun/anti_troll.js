13ba257409ec42353063822a131e04e67ec901f0
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-08-18 21:11:35 -0400

    Create anti_troll.js

---
+++ fun/anti_troll.js
@@ -0,0 +1,27 @@
+// Anti-troll starblast
+
+/*
+  Prevents excessive trolling of players by means of unprecedented killing, basecamping, etc
+  although this is vulnerable to high damage burst shots, it at least protects the player
+  from accidently killing themselves, or getting killed by a troll against the rules.
+  
+  Have fun and enjoy!
+*/
+
+this.options = {
+  root_mode: "survival",
+  map_size: 30
+};
+
+const max_tier = 600;
+
+this.tick = function(game) {
+  for (var ship of game.ships) {
+    if (ship.shield < ship.type / 5 && ship.type < max_tier) {
+      ship.set({
+        invulnerable: 360,
+        shield: ship.shield + 2
+      });
+    }
+  }
+}
