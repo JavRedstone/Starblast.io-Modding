6397978d0b8123819afd6b53e258499fbce0e069
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-08-20 17:49:32 -0400

    Update glitch_screen.js

--- fun/glitch_screen.js
+++ fun/glitch_screen.js
@@ -12,26 +12,22 @@ this.options = {
   map_size: 30
 };
 
-const x = 5;
-const y = 5;
-
-const opacity = 5;
+const x = 3;
+const y = 3;
 
 this.tick = function(game) {
-  for (var ship of game.ships) {
-    if (!ship.custom.ready){
-      ship.custom.ready = true;
+  if (game.step % 150 === 0){
+    for (var ship of game.ships) {
       for (let i = 0; i < 100; i+=y) {
         for (let j = 0; j < 100; j+=x) {
-          var fill_random = [`#f00${opacity}`, `#00f${opacity}`, `#ff0${opacity}`, `#0ff${opacity}`, `#fff${opacity}`, `#0f0${opacity}`];
-          var fill_value = fill_random[Math.round(Math.random() * (fill_random.length - 1))];
+          var fill_value = ["#000","#fff","#fff"];
           ship.setUIComponent({
             id: `${[i, j]}`,
             position: [j,i,x,y],
             clickable: false,
             visible: true,
             components: [
-              { type: "box", position: [0,0,100,100], fill: fill_value},
+              { type: "box", position: [0,0,100,100], fill: fill_value[Math.round(Math.random() * (fill_value.length - 1))]},
             ]
           });
         }
