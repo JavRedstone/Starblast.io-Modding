479edb72bcc3fe85456e5237df17e148687dd740
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-09-12 18:43:04 -0400

    Create wave_four.js

---
+++ utilities/starblast_invasion_simulation/wave_four.js
@@ -0,0 +1,54 @@
+// Starblast invasion wave 4 simulation
+
+this.options = {
+  map_size: 20,
+  custom_map: "",
+  starting_ship: 704
+};
+
+var asteroid_rate = 15;
+var asteroid_counter = 0;
+var asteroid_done;
+
+var alien_rate = 25;
+var alien_counter = 0;
+var alien_done;
+
+this.tick = function(game) {
+  if (game.step === 0) echo("Wave 4 has started...");
+  if (game.step == 500) echo("Preparing to spawn asteroids...");
+  if (game.step > 500 & game.step % asteroid_rate === 0) {
+    if (asteroid_counter < 100) {
+      game.addAsteroid({
+        x: Math.random() * game.options.map_size * 5 * (Math.round(Math.random()) === 0 ? -1 : 1),
+        y: -game.options.map_size * 5,
+        vy: -(1 + Math.random() / 2),
+        size: 20 + Math.random() * 20
+      });
+      asteroid_counter++;
+      echo(`Asteroid [${asteroid_counter}] spawned.`);
+    }
+    else if (!asteroid_done) {
+      asteroid_done = game.step;
+      echo("Asteroid spawn finished.");
+    }
+  }
+  if (game.step - asteroid_done == 500) echo("Preparing to spawn aliens...");
+  if (game.step - asteroid_done > 500 & game.step % alien_rate === 0) {
+    if (alien_counter < 100) {
+      game.addAlien({
+        x: Math.random() * game.options.map_size * 5 * (Math.round(Math.random()) === 0 ? -1 : 1),
+        y: Math.random() * game.options.map_size * 5 * (Math.round(Math.random()) === 0 ? -1 : 1),
+        code: 16,
+        crystal_drop: 10
+      });
+      alien_counter++;
+      echo(`Alien [${alien_counter}] spawned.`);
+    }
+    else if (!alien_done) {
+      alien_done = game.step;
+      echo("Alien spawn finished.");
+    }
+  }
+  if(alien_done && game.aliens.length === 0) echo("Wave 4 finished.");
+}
