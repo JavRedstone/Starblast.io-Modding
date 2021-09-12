44e909dc10f9c901d9d734d75d2bdb4a12f16565
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-09-12 16:15:49 -0400

    Update stacking_aliens.js

--- utilities/stacking_aliens.js
+++ utilities/stacking_aliens.js
@@ -13,7 +13,7 @@ const amount = 10;
 
 this.tick = function(game) {
   // So you can click the link lol
-  if (game.step > 600) {
+  if (game.step > 500) {
     if (game.aliens.length < amount) game.addAlien({code: 10, level: 0, crystal_drop: 20});
     for (let i = 0; i < game.aliens.length; i++) {
       var alien = game.aliens[i];
