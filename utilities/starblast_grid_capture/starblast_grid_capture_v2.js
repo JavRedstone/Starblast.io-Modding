3aa5954e294a6fa0a881a030aba630f7ffd68ba4
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-09-29 10:31:43 -0400

    Update starblast_grid_capture_v2.js

--- utilities/starblast_grid_capture/starblast_grid_capture_v2.js
+++ utilities/starblast_grid_capture/starblast_grid_capture_v2.js
@@ -15,8 +15,27 @@ var control = {
     values: ["🡸", "🡺", "🡹", "🡻"],
     shortcuts: ["A", "D", "W", "S"]
   },
-  started: false,
+  wait: {
+    started: false,
+    player: 4
+  }
 };
+
+function generate_dirs (ship) {
+  for (let i = 0; i < control.dirs.names.length; i++) {
+    ship.setUIComponent({
+      id: control.dirs.names[i],
+      position: [60 + i * 4, 1, 4, 6.4],
+      clickable: ship.custom.dir_rest[i].clickable,
+      shortcut: `${control.dirs.shortcuts[i]}`,
+      components: [
+        { type: "box", position: [0,0,100,100], stroke: ship.custom.dir_rest[i].stroke, width: 2},
+        { type: "text", position: [5, 10, 90, 60], value: control.dirs.values[i], color: ship.custom.dir_rest[i].color},
+        { type: "text", position: [5, 65, 90, 25], value: `[${control.dirs.shortcuts[i]}]`, color: ship.custom.dir_rest[i].color}
+      ]
+    });
+  }
+}
 
 this.tick = function (game) {
   switch (true) {
@@ -24,7 +43,36 @@ this.tick = function (game) {
       
       break;
     case game.step % 30 === 0:
-      
+      switch (control.wait.started) {
+        case true:
+          
+          break;
+        case false:
+          if (game.ships.length >= control.wait.player) {
+            control.wait.started = true;
+          }
+          
+          else {
+            
+          }
+          break;
+      }
+      break;
+  }
+}
+
+this.event = function (event, game) {
+  var ship = event.ship;
+  switch (event.name) {
+    case "ship_spawned":
+      for (let i = 0; i < control.dirs.names.length; i++) {
+        ship.custom.rest.push({
+          clickable: false,
+          stroke: "grey",
+          color: "grey"
+        });
+      }
+      generate_dirs (ship);
       break;
   }
 }
