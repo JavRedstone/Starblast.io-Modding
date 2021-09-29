1ded826492b9758d13786e9dd3fc7c0c87ab887e
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-09-29 08:38:02 -0400

    Update starblast_grid_capture_v2.js

--- utilities/starblast_grid_capture/starblast_grid_capture_v2.js
+++ utilities/starblast_grid_capture/starblast_grid_capture_v2.js
@@ -1,1 +1,30 @@
+this.options = {
+  map_size: 30,
+  custom_map: "",
+  weapons_store: false
+}
 
+var control = {
+  teams: {
+    colors: ["red", "yellow", "lime", "blue"],
+    abbrev: ["R", "Y", "L", "B"],
+    hues: [0, 60, 120, 240]
+  },
+  dirs: {
+    names: ["left", "right", "up", "down"],
+    values: ["🡸", "🡺", "🡹", "🡻"],
+    shortcuts: ["A", "D", "W", "S"]
+  },
+  started: false,
+};
+
+this.tick = function (game) {
+  switch (true) {
+    case game.step === 0:
+      
+      break;
+    case game.step % 30 === 0:
+      
+      break;
+  }
+}
