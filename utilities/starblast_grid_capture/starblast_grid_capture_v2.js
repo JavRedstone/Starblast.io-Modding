786ca32eb97b767dde5e2d47597c12952ee55c02
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-09-29 10:43:34 -0400

    Update starblast_grid_capture_v2.js

--- utilities/starblast_grid_capture/starblast_grid_capture_v2.js
+++ utilities/starblast_grid_capture/starblast_grid_capture_v2.js
@@ -6,11 +6,14 @@ this.options = {
 
 var control = {
   teams: {
+    length: 4,
+    current: 3,
     colors: ["red", "yellow", "lime", "blue"],
     abbrev: ["R", "Y", "L", "B"],
     hues: [0, 60, 120, 240]
   },
   dirs: {
+    length: 4,
     names: ["left", "right", "up", "down"],
     values: ["🡸", "🡺", "🡹", "🡻"],
     shortcuts: ["A", "D", "W", "S"]
@@ -22,13 +25,13 @@ var control = {
 };
 
 function generate_dirs (ship) {
-  for (let i = 0; i < control.dirs.names.length; i++) {
+  for (let i = 0; i < control.dirs.length; i++) {
     ship.setUIComponent({
       id: control.dirs.names[i],
       position: [60 + i * 4, 1, 4, 6.4],
       clickable: ship.custom.dir_rest[i].clickable,
       shortcut: `${control.dirs.shortcuts[i]}`,
-      components: [
+      component.s: [
         { type: "box", position: [0,0,100,100], stroke: ship.custom.dir_rest[i].stroke, width: 2},
         { type: "text", position: [5, 10, 90, 60], value: control.dirs.values[i], color: ship.custom.dir_rest[i].color},
         { type: "text", position: [5, 65, 90, 25], value: `[${control.dirs.shortcuts[i]}]`, color: ship.custom.dir_rest[i].color}
@@ -65,14 +68,32 @@ this.event = function (event, game) {
   var ship = event.ship;
   switch (event.name) {
     case "ship_spawned":
-      for (let i = 0; i < control.dirs.names.length; i++) {
+      for (let i = 0; i < control.dirs.length; i++) {
         ship.custom.rest.push({
           clickable: false,
           stroke: "grey",
           color: "grey"
         });
       }
+      
       generate_dirs (ship);
+      
+      var team_color = control.teams.colors[control.teams.current];
+      var team_abbrev = control.teams.abbrev[control.teams.current];
+      var team_hue = control.teams.hues[control.teams.current];
+      
+      ship.setUIComponent({
+        id: "team",
+        position: [76, 1, 4, 6.4],
+        components: [
+          { type: "box", position: [0,0,100,100], stroke: team_color, width: 2},
+          { type: "text", position: [5, 10, 90, 60], value: team_abbrev, color: team_color},
+          { type: "text", position: [5, 65, 90, 25], value: `[${team_color.toUpperCase()}]`, color: team_color}
+        ]
+      });
+      
+      ship.custom.team = control.teams.current;
+      
       break;
   }
 }
