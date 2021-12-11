86badac31352fdae84ed74f1a132d60a3a686d4f
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2021-12-11 18:19:07 -0500

    Update starblast_bed_wars.js

--- utilities/starblast_bed_wars/starblast_bed_wars.js
+++ utilities/starblast_bed_wars/starblast_bed_wars.js
@@ -1,1 +1,23 @@
+// Start preliminary constants ----------
 
+// End preliminary constants ----------
+
+// Start preliminary functions ----------
+
+// End preliminary functions ----------
+
+this.options = {
+  
+};
+
+// Start functions for this.tick ----------
+
+// End functions for this.tick ----------
+
+this.tick = function () {
+  
+};
+
+this.event = function (event) {
+  
+};
