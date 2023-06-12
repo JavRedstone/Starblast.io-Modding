58cfc62c4e26e7d2fdaf8eff2af08dc034dbda36
JavRedstone <javier.huang168@gmail.com>
2023-06-11 20:27:23 -0400

    Update stacking_aliens.js

--- fun/stacking_aliens.js
+++ fun/stacking_aliens.js
@@ -7,35 +7,31 @@
 this.options = {
   root_mode: "team",
   map_size: 60,
-  crystal_value: 8,
+  crystal_value: 6,
   asteroids_strength: 0.1,
-  station_crystal_capacity: 0.1,
-  station_size: 5,
-  station_regeneration: 2,
+  station_crystal_capacity: 0.75,
+  station_size: 2,
+  station_regeneration: 1.5,
   all_ships_can_dock: true,
-  friendly_colors: 2
+  friendly_colors: 1
 };
 
 const amount = 20;
 
+
+
 this.tick = function(game) {
-  // So you can click the link lol
-  if (game.step > 500) {
-    if (game.aliens.length < amount) game.addAlien({code: 10, level: 0, crystal_drop: 20});
+  if (game.step > 500 && game.step % 5 === 0) {
+    var arr = [10, 11, 14, 16, 17, 18];
+    if (game.aliens.length < amount) game.addAlien({code: arr[Math.floor(Math.random() * arr.length)], level: Math.floor(Math.random() * 3), crystal_drop: 200});
     for (let i = 0; i < game.aliens.length; i++) {
       var alien = game.aliens[i];
       if (i > 0) alien.set({x: game.aliens[0].x, y: game.aliens[0].y, vx: 0, vy: 0});
-      alien.set({rate: 10, damage: 15, laser_speed: 240});
+      alien.set({rate: 10, damage: 20, laser_speed: 250});
     }
-    if (game.step % 3600 === 0) {
-      // game.aliens[0].set({x:(Math.round(Math.random()) == 0 ? 1 : -1) * Math.random() * 60 * 5, y: (Math.round(Math.random()) == 0 ? 1 : -1) * Math.random() * 60 * 5});
-      
-      x = Math.floor(Math.random() * game.ships.length);
     
-      game.ships[x].set({invulnerable:500});
-    
-      game.aliens[0].set({x:game.ships[x].x, y:game.ships[x].y})
-      
+    if (game.step % 1200 === 0) {
+      game.aliens[0].set({x:(Math.round(Math.random()) == 0 ? 1 : -1) * Math.random() * 60 * 5, y: (Math.round(Math.random()) == 0 ? 1 : -1) * Math.random() * 60 * 5});
     }
   }
 }
