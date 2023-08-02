92fc4c88f775107d20ec5b3fb95f1d94cf34a77f
JavRedstone <javier.huang168@gmail.com>
2023-08-02 15:34:49 -0400

    Create notus_object_fix.js

---
+++ utilities/notus_object_fix.js
@@ -0,0 +1,19 @@
+/*
+Issue: when you remove your custom 3D objects, after that newly joined players still can see old "removed" objects.
+
+Fix: add this code to your mod before you set any objects:
+*/
+
+if (!game.setObject.old) {
+  const setObject = function(t) {
+    if (t && t.id != null) { // jshint ignore:line
+      setObject.old.apply(this, arguments);
+      if (this.objects_by_id[t.id] == null) { // jshint ignore:line
+        this.objects_by_id[t.id] = this.objects_by_id.undefined;
+      }
+      delete this.objects_by_id.undefined;
+    }
+  };
+  setObject.old = game.setObject;
+  game.setObject = setObject;
+}
