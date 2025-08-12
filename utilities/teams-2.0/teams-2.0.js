12995282783b22d496c68b0738432406b3a06042
Javier H. <javier.huang168@gmail.com>
2025-08-12 11:00:05 -0400

    Last update with doors

--- utilities/teams-2.0/teams-2.0.js
+++ utilities/teams-2.0/teams-2.0.js
@@ -140,9 +140,9 @@ const Game = class {
                 "3  9  3   8     1             99 99 99  2       3  3       2  99 99 99             1     8   3  9  3\n"+
                 " 36     6         1                                                              1         6     63 ",
 
-            ASTEROIDS_STRENGTH: 1,
+            ASTEROIDS_STRENGTH: 0.75,
             RELEASE_CRYSTAL: true,
-            CRYSTAL_DROP: 1,
+            CRYSTAL_DROP: 2,
             CRYSTAL_VALUE: 2,
 
             FRIENDLY_COLORS: 2,
@@ -211,7 +211,7 @@ const Game = class {
     }
 
     static setShipGroups(shipGroups) {
-        Game.C.OPTIONS.SHIPS = ['{"name":"Invisible","level":1,"model":2,"size":0.1,"zoom":0.1,"next":[],"specs":{"shield":{"capacity":[100,100],"reload":[100,100]},"generator":{"capacity":[1,1],"reload":[1,1]},"ship":{"mass":0,"speed":[1,1],"rotation":[1,1],"acceleration":[1,1]}},"bodies":{"main":{"section_segments":1,"offset":{"x":0,"y":0,"z":0},"position":{"x":[1,0],"y":[0,0],"z":[0,0]},"width":[0,0],"height":[0,0]}},"typespec":{"name":"Invisible","level":1,"model":2,"code":101,"specs":{"shield":{"capacity":[100,100],"reload":[100,100]},"generator":{"capacity":[1,1],"reload":[1,1]},"ship":{"mass":0,"speed":[1,1],"rotation":[1,1],"acceleration":[1,1]}},"shape":[0,0,0,0,0,0,0,0,0,0,0,0,0,0.002,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"lasers":[],"radius":0.002,"next":[]}}'];
+        Game.C.OPTIONS.SHIPS = ['{"name":"Invisible","level":1,"model":2,"size":0.1,"zoom":0.1,"next":[],"specs":{"shield":{"capacity":[100,100],"reload":[100,100]},"generator":{"capacity":[1,1],"reload":[1,1]},"ship":{"mass":0,"speed":[1,1],"rotation":[1,1],"acceleration":[1,1]}},"bodies":{"main":{"section_segments":1,"offset":{"x":0,"y":0,"z":0},"position":{"x":[1,0],"y":[0,0],"z":[0,0]},"width":[0,0],"height":[0,0]}},"typespec":{"name":"Invisible","level":1,"model":2,"code":102,"specs":{"shield":{"capacity":[100,100],"reload":[100,100]},"generator":{"capacity":[1,1],"reload":[1,1]},"ship":{"mass":0,"speed":[1,1],"rotation":[1,1],"acceleration":[1,1]}},"shape":[0,0,0,0,0,0,0,0,0,0,0,0,0,0.002,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"lasers":[],"radius":0.002,"next":[]}}'];
         for (let [tier, shipMap] of Object.entries(shipGroups)) {
             let shipGroup = new ShipGroup(parseInt(tier), shipMap);
             Game.shipGroups.push(shipGroup);
@@ -1336,16 +1336,17 @@ const Game = class {
         if (ship.isDonating) {
             if (ship.ship.crystals > 0) {
                 let shipLevel = ship.getLevel();
-                let donateAmount = Math.round(shipLevel * DepotBaseModule.C.DONATE_SPEED_MULTIPLIER * Math.pow(ship.team.base.baseUpgrades[5].default.MULTIPLIER, ship.team.base.baseUpgrades[5].level));
+                let donateAmount = Math.round(shipLevel * DepotBaseModule.C.DONATE_SPEED_MULTIPLIER);
                 let realDonateAmount = Math.min(donateAmount, ship.ship.crystals);
                 ship.setCrystals(Math.max(0, ship.ship.crystals - realDonateAmount));
-                ship.credits += realDonateAmount;
-                ship.totalContributed += realDonateAmount;
-                ship.setScore(ship.ship.score + realDonateAmount);
-                ship.lastContributedAmount += realDonateAmount;
+                let creditConversionAmount = realDonateAmount * Math.pow(ship.team.base.baseUpgrades[5].default.MULTIPLIER, ship.team.base.baseUpgrades[5].level);
+                ship.credits += creditConversionAmount;
+                ship.totalContributed += creditConversionAmount;
+                ship.setScore(ship.ship.score + creditConversionAmount);
+                ship.lastContributedAmount += creditConversionAmount;
                 if (ship.team) {
                     if (!ship.team.base.dead) {
-                        ship.team.base.crystals += realDonateAmount;
+                        ship.team.base.crystals += creditConversionAmount;
                         if (ship.team.base.crystals >= Base.C.MAX_CRYSTALS[ship.team.base.baseLevel - 1]) {
                             if (ship.team.base.baseLevel < 4) {
                                 ship.team.base.baseLevel += 1;
@@ -1855,8 +1856,8 @@ const ShipGroup = class {
     static C = {
         SHIPS: {
             '1': {
-                '101': { // When adding fly, add the `next: [201, 202] fields`
-                    SHIP: '{"name":"Fly","level":1,"model":1,"size":1.05,"next":[201,202],"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[125,145],"rotation":[110,130],"acceleration":[100,120]}},"bodies":{"main":{"section_segments":12,"offset":{"x":0,"y":0,"z":10},"position":{"x":[0,0,0,0,0,0,0,0,0,0],"y":[-65,-60,-50,-20,10,30,55,75,60],"z":[0,0,0,0,0,0,0,0,0]},"width":[0,8,10,30,25,30,18,15,0],"height":[0,6,8,12,20,20,18,15,0],"propeller":true,"texture":[4,63,10,1,1,1,12,17]},"cockpit":{"section_segments":12,"offset":{"x":0,"y":0,"z":20},"position":{"x":[0,0,0,0,0,0,0],"y":[-15,0,20,30,60],"z":[0,0,0,0,0]},"width":[0,13,17,10,5],"height":[0,18,25,18,5],"propeller":false,"texture":[7,9,9,4,4]},"cannon":{"section_segments":6,"offset":{"x":0,"y":-15,"z":-10},"position":{"x":[0,0,0,0,0,0],"y":[-40,-50,-20,0,20,30],"z":[0,0,0,0,0,20]},"width":[0,5,8,11,7,0],"height":[0,5,8,11,10,0],"angle":0,"laser":{"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"error":2.5},"propeller":false,"texture":[3,3,10,3]}},"wings":{"main":{"length":[60,20],"width":[100,50,40],"angle":[-10,10],"position":[0,20,10],"doubleside":true,"offset":{"x":0,"y":10,"z":5},"bump":{"position":30,"size":20},"texture":[11,63]}},"typespec":{"name":"Fly","level":1,"model":1,"code":101,"next":[201,202],"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[125,145],"rotation":[110,130],"acceleration":[100,120]}},"shape":[1.368,1.368,1.093,0.965,0.883,0.827,0.791,0.767,0.758,0.777,0.847,0.951,1.092,1.667,1.707,1.776,1.856,1.827,1.744,1.687,1.525,1.415,1.335,1.606,1.603,1.578,1.603,1.606,1.335,1.415,1.525,1.687,1.744,1.827,1.856,1.776,1.707,1.667,1.654,0.951,0.847,0.777,0.758,0.767,0.791,0.827,0.883,0.965,1.093,1.368],"lasers":[{"x":0,"y":-1.365,"z":-0.21,"angle":0,"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"spread":0,"error":2.5,"recoil":0}],"radius":1.856}}',
+                '102': { // When adding fly, add the `next: [201, 202] fields`
+                    SHIP: '{"name":"Fly","level":1,"model":1,"size":1.05,"next":[201,202],"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[125,145],"rotation":[110,130],"acceleration":[100,120]}},"bodies":{"main":{"section_segments":12,"offset":{"x":0,"y":0,"z":10},"position":{"x":[0,0,0,0,0,0,0,0,0,0],"y":[-65,-60,-50,-20,10,30,55,75,60],"z":[0,0,0,0,0,0,0,0,0]},"width":[0,8,10,30,25,30,18,15,0],"height":[0,6,8,12,20,20,18,15,0],"propeller":true,"texture":[4,63,10,1,1,1,12,17]},"cockpit":{"section_segments":12,"offset":{"x":0,"y":0,"z":20},"position":{"x":[0,0,0,0,0,0,0],"y":[-15,0,20,30,60],"z":[0,0,0,0,0]},"width":[0,13,17,10,5],"height":[0,18,25,18,5],"propeller":false,"texture":[7,9,9,4,4]},"cannon":{"section_segments":6,"offset":{"x":0,"y":-15,"z":-10},"position":{"x":[0,0,0,0,0,0],"y":[-40,-50,-20,0,20,30],"z":[0,0,0,0,0,20]},"width":[0,5,8,11,7,0],"height":[0,5,8,11,10,0],"angle":0,"laser":{"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"error":2.5},"propeller":false,"texture":[3,3,10,3]}},"wings":{"main":{"length":[60,20],"width":[100,50,40],"angle":[-10,10],"position":[0,20,10],"doubleside":true,"offset":{"x":0,"y":10,"z":5},"bump":{"position":30,"size":20},"texture":[11,63]}},"typespec":{"name":"Fly","level":1,"model":1,"code":101,"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[125,145],"rotation":[110,130],"acceleration":[100,120]}},"shape":[1.368,1.368,1.093,0.965,0.883,0.827,0.791,0.767,0.758,0.777,0.847,0.951,1.092,1.667,1.707,1.776,1.856,1.827,1.744,1.687,1.525,1.415,1.335,1.606,1.603,1.578,1.603,1.606,1.335,1.415,1.525,1.687,1.744,1.827,1.856,1.776,1.707,1.667,1.654,0.951,0.847,0.777,0.758,0.767,0.791,0.827,0.883,0.965,1.093,1.368],"lasers":[{"x":0,"y":-1.365,"z":-0.21,"angle":0,"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"spread":0,"error":2.5,"recoil":0}],"radius":1.856,"next":[201,202]}}',
                     HITBOX: { CENTER: { x: 0, y: 0 }, SIZE: { x: 3, y: 3 } },
                     ABBREVIATION: 'FLY'
                 },
@@ -2826,7 +2827,7 @@ const Base = class {
     safeAliens = [];
 
     crystals = 0;
-    baseLevel = 1;
+    baseLevel = 4;
     dead = false;
     reachedMaxLevel = false;
     maxRecords = [];
@@ -3954,7 +3955,7 @@ const DoorBaseModule = class extends BaseModule {
     lastOpenedTime = -1;
 
     static C = {
-        NUM_DOORS: 2,
+        NUM_DOORS: 1,
         DOOR_HEIGHT: 2.5,
         DOOR_CYCLE_STEPS: 5,
         CLOSING_DIFF: 0.25,
@@ -4949,9 +4950,9 @@ const Obj = class {
                     z: 0
                 },
                 scale: {
-                    x: 0.1,
+                    x: 0.05,
                     y: 0.25,
-                    z: 4
+                    z: 3
                 },
                 type: {
                     id: 'door',
@@ -5960,6 +5961,19 @@ const UISubComponent = class {
     setAlign(align) {
         this.uiSubComponent.align = align;
         return this;
+    }
+}
+
+const GameMap = class {
+    name = '';
+    author = '';
+    map = '';
+    orbitRadius = 0;
+
+    static C = {
+        MAP_GENERATOR: {
+            
+        }
     }
 }
 
